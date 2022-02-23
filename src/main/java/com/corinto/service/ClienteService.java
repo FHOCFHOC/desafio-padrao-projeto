b447f436df6919f907e9ca6c93edcc78f4ec5e90
FHOCFHOC <backintheusa@gmail.com>
2022-02-23 19:44:41 -0300

    first commit

---
+++ src/main/java/com/corinto/service/ClienteService.java
@@ -0,0 +1,18 @@
+package com.corinto.service;
+
+import com.corinto.model.Cliente;
+
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
